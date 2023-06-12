969ad21fc6548bc6a2d04c004109c13a9b66535d
Joel Scheuner <joel.scheuner.dev@gmail.com>
2023-06-12 09:34:03 +0200

    Add output channel logging

--- src/lambda/invokeCommand.ts
+++ src/lambda/invokeCommand.ts
@@ -1,7 +1,11 @@
 import * as vscode from 'vscode';
 import { execShell } from '../utils/shell';
+import { logOutputChannel, showLogOutputChannel } from '../utils/outputChannel';
 
 export async function invokeLambda() {
     const stdout = await execShell('cd /Users/joe/Projects/Lambda-IDE-Integration/lambda-python && make invoke');
-    vscode.window.showInformationMessage(`Invoke Lambda function in LocalStack:\n${stdout}`);
+    vscode.window.showInformationMessage(`Invoked Lambda function in LocalStack.`);
+
+    logOutputChannel.appendLine(stdout);
+    showLogOutputChannel();
 }
