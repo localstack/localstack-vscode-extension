f3a89274fa4ef912096581e57a972983644095cd
Misha Tiurin <misha.tiurin@localstack.cloud>
2025-09-01 13:02:00 +0200

    Remove 0.2.0 preview code

--- src/lambda/infoCommand.ts
+++
@@ -1,7 +0,0 @@
-import * as vscode from 'vscode';
-import { execShell } from '../utils/shell';
-
-export async function showInformationMessage() {
-    const stdout = await execShell('sam --version');
-    vscode.window.showInformationMessage(`sam version: ${stdout}`);
-}
