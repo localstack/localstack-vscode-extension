93932f92a60c281099bb344ae929c190fa020563
Cristian Pallarés <cristian.pallares@localstack.cloud>
2025-09-04 12:18:07 +0200

    wip

--- src/utils/time-tracker.ts
+++ src/utils/time-tracker.ts
@@ -13,22 +13,6 @@ export const createTimeTracker = (options: {
 }): TimeTracker => {
 	return {
 		run<T>(name: string, fn: () => T): T {
-			// options.outputChannel.trace(`[time-tracker]: Starting "${name}"...`);
-			// const start = Date.now();
-			// try {
-			// 	const result = fn();
-			// 	const end = Date.now();
-			// 	options.outputChannel.trace(
-			// 		`[time-tracker]: "${name}" completed in ${timeDiff(start, end)}`,
-			// 	);
-			// 	return result;
-			// } catch (error) {
-			// 	const end = Date.now();
-			// 	options.outputChannel.error(
-			// 		`[time-tracker]: "${name}" failed in ${timeDiff(start, end)}`,
-			// 	);
-			// 	throw error;
-			// }
 			options.outputChannel.trace(`[${name}]: Starting...`);
 			const start = Date.now();
 			try {
