5fcb406daaa2fcfe51df04388f6c467800fc373d
Anisa Oshafi <anisaoshafi@gmail.com>
2025-09-09 00:04:42 +0200

    chore: log DNS resolution failure

--- src/plugins/configure-aws.ts
+++ src/plugins/configure-aws.ts
@@ -3,13 +3,17 @@ import { commands } from "vscode";
 import { createPlugin } from "../plugins.ts";
 import { configureAwsProfiles } from "../utils/configure-aws.ts";
 
-export default createPlugin("configure-aws", ({ context, telemetry }) => {
-	context.subscriptions.push(
-		commands.registerCommand("localstack.configureAwsProfiles", async () => {
-			await configureAwsProfiles({
-				telemetry: telemetry,
-				notifyNoChangesMade: true,
-			});
-		}),
-	);
-});
+export default createPlugin(
+	"configure-aws",
+	({ context, telemetry, outputChannel }) => {
+		context.subscriptions.push(
+			commands.registerCommand("localstack.configureAwsProfiles", async () => {
+				await configureAwsProfiles({
+					telemetry: telemetry,
+					notifyNoChangesMade: true,
+					outputChannel,
+				});
+			}),
+		);
+	},
+);
