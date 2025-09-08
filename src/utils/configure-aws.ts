f0444b14e837823d2c8d235792b681d366cef45c
Anisa Oshafi <anisaoshafi@gmail.com>
2025-09-08 12:54:03 +0200

    Update src/utils/configure-aws.ts
    Co-authored-by: Cristian Pallarés <cristian.pallares@localstack.cloud>

--- src/utils/configure-aws.ts
+++ src/utils/configure-aws.ts
@@ -57,7 +57,7 @@ function isValidEndpointUrl(url: string | undefined): boolean {
 		return (
 			(parsed.protocol === "http:" || parsed.protocol === "https:") &&
 			VALID_HOSTNAMES.includes(parsed.hostname) &&
-			parsed.port // port must be present
+			parsed.port !== "" // port must be present
 		);
 	} catch {
 		return false;
