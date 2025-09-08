574ac736abb27b90925c1eb7fd8e6f83f9622571
Cristian Pallarés <cristian.pallares@localstack.cloud>
2025-09-08 16:31:03 +0200

    fix

--- src/extension.ts
+++ src/extension.ts
@@ -53,7 +53,7 @@ export async function activate(context: ExtensionContext) {
 		);
 		context.subscriptions.push(containerStatusTracker);
 
-		const localStackStatusTracker = await createLocalStackStatusTracker(
+		const localStackStatusTracker = createLocalStackStatusTracker(
 			containerStatusTracker,
 			outputChannel,
 			timeTracker,
