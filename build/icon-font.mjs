8eb64b0cd33b279f27a14defdc99955736ab6f99
Cristian Pallarés <cristian.pallares@localstack.cloud>
2025-09-09 09:35:23 +0200

    chore: npm audit (#42)
    Updated dependencies, and ran `npm audit` to fix vulnerabilities.
    
    One of our development dependencies (`webfont`, used to build our custom
    VS Code icons) is affected, so we had to disable temporarily our
    `build/icon-font.mjs` script.

--- build/icon-font.mjs
+++ build/icon-font.mjs
@@ -1,31 +1,35 @@
-import fs from "node:fs/promises";
-import path from "node:path";
+// DANGER: This file is ignored because it requires the "webfont" package, which has
+// been compromised to some extent through https://www.aikido.dev/blog/npm-debug-and-chalk-packages-compromised.
+// Ideally, we will replace this with a safer alternative in the future, or restore when the package is fixed.
 
-import { webfont } from "webfont";
+// import fs from "node:fs/promises";
+// import path from "node:path";
 
-const svgsDir = path.resolve(`${import.meta.dirname}/../resources/icons`);
-const svgs = await fs
-	.readdir(svgsDir)
-	.then((files) => files.filter((file) => file.endsWith(".svg")))
-	.then((files) => files.map((file) => `${svgsDir}/${file}`));
+// import { webfont } from "webfont";
 
-const dest = `${import.meta.dirname}/../resources/fonts/localstack.woff`;
+// const svgsDir = path.resolve(`${import.meta.dirname}/../resources/icons`);
+// const svgs = await fs
+// 	.readdir(svgsDir)
+// 	.then((files) => files.filter((file) => file.endsWith(".svg")))
+// 	.then((files) => files.map((file) => `${svgsDir}/${file}`));
 
-async function generateFont() {
-	try {
-		const result = await webfont({
-			files: svgs,
-			formats: ["woff"],
-			startUnicode: 0xe000,
-			normalize: true,
-			sort: false,
-		});
-		await fs.mkdir(path.dirname(dest), { recursive: true });
-		await fs.writeFile(dest, result.woff, "binary");
-		console.log(`Font created at ${path.relative(process.cwd(), dest)}`);
-	} catch (error) {
-		console.error("Font creation failed.", error);
-	}
-}
+// const dest = `${import.meta.dirname}/../resources/fonts/localstack.woff`;
 
-await generateFont();
+// async function generateFont() {
+// 	try {
+// 		const result = await webfont({
+// 			files: svgs,
+// 			formats: ["woff"],
+// 			startUnicode: 0xe000,
+// 			normalize: true,
+// 			sort: false,
+// 		});
+// 		await fs.mkdir(path.dirname(dest), { recursive: true });
+// 		await fs.writeFile(dest, result.woff, "binary");
+// 		console.log(`Font created at ${path.relative(process.cwd(), dest)}`);
+// 	} catch (error) {
+// 		console.error("Font creation failed.", error);
+// 	}
+// }
+
+// await generateFont();
